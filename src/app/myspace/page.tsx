bdb785dae14098ebb8a31099ab50486436aeed0e
eisemsi2 <prajjwalk21@iitk.ac.in>
2024-05-24 21:50:11 +0530

    added messages

--- src/app/myspace/page.tsx
+++ src/app/myspace/page.tsx
@@ -7,6 +7,16 @@ export default function MySpace() {
         <div className="flex flex-row">
             <SideBarLeft />
             <SideBarRight />
+            
+            <div className="m-4"> 
+                grids to be placed here <br />
+                to add a form to add new coding platform <br />
+                <span className="font-bold">
+                    to move this div left if the sidebarleft is closed
+                </span>
+
+            </div>
+
         </div>
 
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
-import Image from "next/image";
-import Grid from "./ui/grid";
 import {NavbarMain} from "./ui/navbars";
+import Link from "next/link";
 
 export default function Home() {
     return (
       <>
         <NavbarMain />
-        <div className="px-4">
-            Hello
+        <div className=" px-4 py-6 text-center">
+            to add about this site here <br />
+            on successfull login, redirect to myspace <br />
+            on unsuccessfull login, show error message and bring back here. <br />
+            for now go to myspace from here <Link href="/myspace" className="underline">MySpace</Link>
         </div>
       </>
     );
